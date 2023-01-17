ae97909530a034d1b99cbd1248b7db6acf155cb0
omkarpalkar89 <omkar.palkar89@gmail.com>
2023-01-17 20:58:26 +0530

    Update util.py

--- nse_fno_expiry_calculator/util.py
+++ nse_fno_expiry_calculator/util.py
@@ -3,19 +3,25 @@ from pendulum.date import Date
 import logging
 
 listOfNseHolidays = set([
-    Date(2022, 1, 26),  # Republic Day
-    Date(2022, 3, 1),  # Maha Shivaratri
-    Date(2022, 3, 18),  # Holi
-    Date(2022, 4, 14),  # Dr.Baba Saheb Ambedkar Jayanti
-    Date(2022, 4, 15),  # Good friday
-    Date(2022, 5, 3),  # Id-ul-Fitr
-    Date(2022, 8, 9),  # Moharram
-    Date(2022, 8, 15),  # Independence Day
-    Date(2022, 8, 31),  # Ganesh Chaturthi
-    Date(2022, 10, 5),  # Vijaya Dashami
-    Date(2022, 10, 24),  # Diwali-Laxmi Pujan
-    Date(2022, 10, 26),  # Diwali-Balipratipada
-    Date(2022, 11, 8)   # Guru Nanak Jayanti
+Date(2023,1,26),
+Date(2023,3,7),
+Date(2023,3,22),
+Date(2023,3,30),
+Date(2023,4,4),
+Date(2023,4,7),
+Date(2023,4,14),
+Date(2023,5,1),
+Date(2023,5,5),
+Date(2023,6,28),
+Date(2023,8,15),
+Date(2023,8,16),
+Date(2023,9,19),
+Date(2023,9,28),
+Date(2023,10,2),
+Date(2023,10,24),
+Date(2023,11,14),
+Date(2023,11,27),
+Date(2023,12,25)
 ])
 
 def getNearestWeeklyExpiryDate():
